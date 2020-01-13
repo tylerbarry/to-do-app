aadb67b52da8325689eb6a2953d160c5ceebccf0
Tyler Barry <tylerjbarry@gmail.com>
2020-01-13 14:57:19 -0500

    cp-13-DOM

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 function onReady(){
-  const ADD_TODO_FORM = document.getElementById('addToDoForm');
-  const NEW_TODO_TEXT = document.getElementById('newToDoText');
-  const TODO_LIST = document.getElementById('toDoList');
+  const toDos = [];
+  const addToDoForm = document.getElementByID('addToDoForm');
 
-  ADD_TODO_FORM.addEventListener('submit', (event) => {
+  function createNewToDo() {
+    const newToDoText = document.getElementByID('newToDoText');
+    if (!newToDoText.value) { return; }
+    
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value='';
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementByID('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    let title = NEW_TODO_TEXT.value;
-
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    let newLi = document.createElement('li');
-
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.addEventListener('click', function(event){
-      TODO_LIST.removeChild(this.parentElement);
-});
-
-    newLi.textContent = title;
-    newLi.className += "mdl-list__item";
-    newLi.appendChild(checkbox);
-    newLi.appendChild(deleteBtn);
-    deleteBtn.className += "mdl-button mdl-js-button mdl-button--raised mdl-button--colored";
-    checkbox.className += "mdl-checkbox mdl-js-checkbox";
-    TODO_LIST.appendChild(newLi);
-    NEW_TODO_TEXT.value = "Add a Task";
+    createNewToDo();
   });
-};
 
-window.onload = function(){
+  renderTheUI();
+}
+
+window.onload = function() {
   onReady();
 };
