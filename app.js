b1e89c8bc05ef3d0b13c719494510fdaec3aa9f9
tylerbarry <53415989+tylerbarry@users.noreply.github.com>
2020-01-14 12:11:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
-function onReady(){
-  const addToDoForm = document.getElementByID('addToDoForm');
-  let toDos = [];
-  let id = 0;
+
+function onReady() {
+
+  var toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0
 
   function createNewToDo() {
-    const newToDoText = document.getElementByID('newToDoText');
-    if (!newToDoText.value) { return; }
-
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {
+      return;
+    }
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
       id: id
+
     });
-
     id++;
-
-    newToDoText.value='';
+    newToDoText.value = '';
     renderTheUI();
+
   }
 
   function renderTheUI() {
-    const toDoList = document.getElementByID('toDoList');
-
+    const toDoList = document.getElementById('toDoList');
     toDoList.textContent = '';
-
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const del = document.createElement('Button');
+
+      del.textContent = "Delete";
       checkbox.type = "checkbox";
 
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = "Delete";
-
-      deleteButton.addEventListener('click', event => {
-        toDos = toDos.filter(function(item){
+      del.addEventListener('click', event => {
+        toDos = toDos.filter(function(item) {
           return item.id !== toDo.id;
-        })
+        });
         renderTheUI();
       });
-
       newLi.textContent = toDo.title;
-
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-      newLi.appendChild(deleteButton);
-    });
-  }
+      newLi.appendChild(del);
 
+
+
+    });
+
+
+  }
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
-
   renderTheUI();
 }
 
